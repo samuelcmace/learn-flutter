34cfbf25265cd2cfddab9aa8af628818d5cd9207
Samuel Mace <samuelcmace@gmail.com>
2025-07-07 22:22:15 +0700

    Add AppBar, Padding, and Image to I am Rich App

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text("Hello World"))));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[500],
+        appBar: AppBar(
+          title: Text('I am Rich', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(64.0),
+          child: Center(
+            child: Image(
+              image: NetworkImage(
+                'https://cdn.mos.cms.futurecdn.net/57jQMDN5MZLYfV8ps8HuZQ.jpg',
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
